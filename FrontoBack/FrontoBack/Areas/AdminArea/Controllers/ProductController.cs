bd5520c540c36e0b3a815e0d36cab21a235c5bee
Rufat <rufat@Rufats-MacBook-Air.local>
2023-09-27 18:59:49 +0400

    feat:I added ceate and elete section part of project to my gitHub repo

---
+++ FrontoBack/FrontoBack/Areas/AdminArea/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FrontoBack.Areas.AdminArea.ViewModel;
+using FrontoBack.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FrontoBack.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(AppDbContext context,IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+
+            return View(_context.Products.Include(p=>p.Catagories).ToList());
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create(CreateProductVM createProductVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            if (_context.Products.Any(p=>p.Name.ToLower()==createProductVM.Name.ToLower()))
+            {
+                ModelState.AddModelError("Name", "Name must be unique");
+            }
+
+            return RedirectToAction("Index", "Product");
+        }
+    }
+}
+
---
+++ FrontoBack/FrontoBack/Areas/AdminArea/Controllers/SliderController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FrontoBack.Areas.AdminArea.ViewModel;
+using FrontoBack.DAL;
+using FrontoBack.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FrontoBack.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class SliderController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        // GET: /<controller>/
+        public SliderController(AppDbContext context,IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+
+            return View(_context.Sliders.ToList()) ;
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create(CreateSliderVM createSliderVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            if (!createSliderVM.Image.ContentType.Contains("image"))
+            {
+                ModelState.AddModelError("Image", "Only Image");
+                return View();
+            }
+            else if (createSliderVM.Image.Length/1024>1000)
+            {
+                ModelState.AddModelError("Image", "max size must be 1kb");
+                return View();
+            }
+            string fileName = Guid.NewGuid().ToString() + createSliderVM.Image.FileName;
+            string path = Path.Combine(_webHostEnvironment.WebRootPath, "img", fileName);
+            using(FileStream stream=new FileStream(path, FileMode.Create))
+            {
+                createSliderVM.Image.CopyTo(stream);
+            }
+            _context.Add(new Slider { ImgSrc = fileName });
+            _context.SaveChanges();
+            return RedirectToAction("Index","Slider");
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id==null)
+            {
+                return BadRequest();
+            }
+            Slider slider = _context.Sliders.FirstOrDefault(s => s.Id == id);
+             if (slider==null)
+            {
+                return NotFound();
+            }
+            string path = Path.Combine(_webHostEnvironment.WebRootPath, "img", slider.ImgSrc);
+           
+            System.IO.File.Delete(path);
+            
+            _context.Sliders.Remove(slider);
+            _context.SaveChanges();
+            
+            return RedirectToAction("Index","Slider");
+        }
+    }
+}
+
